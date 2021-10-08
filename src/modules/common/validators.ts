219b8dc44093d047a67feefc1fcf241d257feae6
bterpinc <borut.terpinc@kalmia.si>
2021-10-08 14:09:41 +0200

    Conditional presence val. properly handles bool.

--- src/modules/common/validators.ts
+++ src/modules/common/validators.ts
@@ -22,13 +22,19 @@ export function enumInclusionValidator(enumerator: any, allowNull = false) {
 
 
 /**
+ * Validates the presence of fields in current object. 
+ * If field is not empty it will return true.
  * 
- * @param fieldNames 
- * @returns 
+ * @param fieldNames to be validated
+ * @returns True if field not empty
  */
 export function conditionalPresenceValidator(fieldNames: string[]) {
   return async function (this: any) {
     for (const fieldName of fieldNames) {
+      // This handles boolean value. When the field value is explicity set to false.
+      if (this[fieldName] === false) {
+        return true;
+      }
       if (this[fieldName]) {
         return true;
       }
