dc77ad6260bcbd3229b59c5553b0e63c5338ba2c
bterpinc <borut.terpinc@kalmia.si>
2021-10-11 01:46:12 +0200

    Date manipulation fixes

--- src/modules/datetime/date-manipulations.ts
+++ src/modules/datetime/date-manipulations.ts
@@ -1,14 +1,17 @@
 export class DateTimeHelper {
   public static addHours(currDate: Date, hours: number): Date {
-    currDate.setTime(currDate.getTime() + hours * 60 * 60 * 1000);
-    return currDate;
+    const newDate = new Date(currDate);
+    newDate.setTime(newDate.getTime() + hours * 60 * 60 * 1000);
+    return newDate;
   }
   public static addDays(currDate: Date, days: number): Date {
-    currDate.setTime(currDate.getTime() + days * 24 * 60 * 60 * 1000);
-    return currDate;
+    const newDate = new Date(currDate);
+    newDate.setTime(newDate.getTime() + days * 24 * 60 * 60 * 1000);
+    return newDate;
   }
   public static addMinutes(currDate: Date, minutes: number): Date {
-    currDate.setTime(currDate.getTime() + minutes * 60 * 1000);
-    return currDate;
+    const newDate = new Date(currDate);
+    newDate.setTime(newDate.getTime() + + minutes * 60 * 1000);
+    return newDate;
   }
 }
